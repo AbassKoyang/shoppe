da4c2401d7dd4bcac97e337bb0d82f668d11e8f1
Abass Koyang Ahmad <abasskoyang12345@gmail.com>
2025-10-06 18:13:44 +0100

    ui fix

--- components/ProductPageSkeleton.tsx
+++ components/ProductPageSkeleton.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ProductPageSkeleton = () => {
   return (
-    <div className='col-span-1 row-span-1 bg-gray-200 animate-pulse rounded-[9px] p-1.5 shadow-[0_5px_10px_0_rgba(0,0,0,0.12)]'>
+    <div className='w-full bg-gray-200 animate-pulse'>
         <div className="w-full h-[439px] rounded-b-2xl bg-gray-300 animate-pulse"></div>
         <div className="w-[40%] h-6 rounded-[5px] bg-gray-300 animate-pulse mt-2"></div>
         <div className="w-[50%] h-5 rounded-[5px] bg-gray-300 animate-pulse mt-2"></div>
