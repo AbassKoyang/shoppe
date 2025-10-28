3c73d1a45649d2b245652daf8285d275d31153a7
Abass Koyang Ahmad <abasskoyang12345@gmail.com>
2025-10-28 13:28:09 +0100

    fix error in search page

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,3 +1,4 @@
+import Discover from '@/components/home-page/Discover'
 import ProductCard from '@/components/ProductCard'
 import SearchHeader from '@/components/SearchHeader'
 import React from 'react'
@@ -6,23 +7,7 @@ const page = () => {
   return (
     <section className="w-full min-h-full overflow-x-hidden">
         <SearchHeader />
-        <section className="w-full mt-8 mb-[300px]">
-        <div className="w-full">
-          <h3 className="text-[22px] font-raleway font-bold text-[#202020]">Discover</h3>
-        </div>
-
-        <div className="min-w-full mt-2">
-        <div className="w-full overflow-x-auto flex items-start gap-1.5 carousel-container scrollbar-hide">
-              <ProductCard/>
-              <ProductCard/>
-              <ProductCard/>
-              <ProductCard/>
-              <ProductCard/>
-              <ProductCard/>
-              <ProductCard/>
-          </div>
-        </div>
-      </section>
+        <Discover/>
     </section>
   )
 }
