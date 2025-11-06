672b8857bef49cfbd9199179f38c5f9372ce0d4c
Abass Koyang Ahmad <abasskoyang12345@gmail.com>
2025-11-07 00:40:25 +0100

    god abeg

--- lib/mastra-client.ts
+++ lib/mastra-client.ts
@@ -1,5 +1,5 @@
 import { MastraClient } from "@mastra/client-js";
 
 export const mastraClient = new MastraClient({
-  baseUrl: process.env.NEXT_MASTRA_API_URL!,
+  baseUrl: process.env.NEXT_MASTRA_API_URL || 'https://shoppe-ai-agent-2.onrender.com',
 });
